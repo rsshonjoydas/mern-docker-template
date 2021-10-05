e7fd6c04b463aee31fdb1a3e89cce03a08e332c1
rsshonjoydas <rsshonjoydas@gmail.com>
2021-10-05 12:10:15 +0600

    navbar

--- client/src/layout/NavBar.tsx
+++ client/src/layout/NavBar.tsx
@@ -1,10 +1,25 @@
+import { Link } from "react-router-dom";
+import menuItems from "../api/header.data";
 import IPageProps from "../interfaces/page";
 
 const NavBar: React.FunctionComponent<IPageProps> = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="navbar justify-between">
+      <Link to="/" className="link site-title">
+        RS
+      </Link>
+      <ul className="display-f">
+        {menuItems.map((menuItem, index) => (
+          <Link
+            key={index}
+            to={menuItem.path}
+            className="link text-gray ml-1 text-hover-secondary"
+          >
+            {menuItem.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
