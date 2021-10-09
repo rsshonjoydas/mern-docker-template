b12f6c81ac05758d7af956793a01466d3f33ca9f
rsshonjoydas <rsshonjoydas@gmail.com>
2021-10-10 02:44:44 +0600

    side nav

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -2,9 +2,12 @@ import IPageProps from "../interfaces/page";
 
 const Dashboard: React.FunctionComponent<IPageProps> = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <h1>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim libero
+      nobis, facilis, molestias, assumenda dicta molestiae eligendi facere
+      soluta quidem commodi deserunt vero magnam suscipit non laudantium quo
+      ipsam! Id!
+    </h1>
   );
 };
 
